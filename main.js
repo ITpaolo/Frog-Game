d96589ad5bcea05a3c9b89e1bb4ee69a3fbf22a6
Paolo Chiavino <paolo.chiavino@itspoon.com>
2017-01-16 15:05:06 +0100

    Added a whole new design

---
+++ main.js
@@ -0,0 +1,36 @@
+// Global Variables
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game'),
+  Main = function () {},
+  gameOptions = {
+    playSound: true,
+    playMusic: true
+  },
+  musicPlayer;
+
+
+
+
+Main.prototype = {
+
+  preload: function () {
+    game.load.image('stars',    'assets/images/stars.jpg');
+    game.load.image('loading',  'assets/images/loading.png');
+    game.load.image('brand',    'assets/images/logo.png');
+    game.load.image('floor', 'assets/images/floor.png');
+    game.load.spritesheet('water', 'assets/images/water_animated.png', 32, 32);
+    game.load.spritesheet('frog', 'assets/images/frog.png', 32, 32);
+    game.load.script('polyfill',   'lib/polyfill.js');
+    game.load.script('utils',   'lib/utils.js');
+    game.load.script('splash',  'states/Splash.js');
+    game.load.script('swipe',  'swipe.js');
+  },
+
+  create: function () {
+    game.state.add('Splash', Splash);
+    game.state.start('Splash');
+  }
+
+};
+
+game.state.add('Main', Main);
+game.state.start('Main');
---
+++ states/Game.js
@@ -0,0 +1,178 @@
+var Game = function(game) {};
+
+Game.prototype = {
+
+    preload: function () {
+        this.optionCount = 1;
+    },
+
+    addMenuOption: function (text, callback) {
+        var optionStyle = {font: '30pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', srokeThickness: 4};
+        var txt = game.add.text(game.world.centerX, (this.optionCount * 80) + 200, text, optionStyle);
+        txt.anchor.setTo(0.5);
+        txt.stroke = "rgba(0,0,0,0";
+        txt.strokeThickness = 4;
+        var onOver = function (target) {
+            target.fill = "#FEFFD5";
+            target.stroke = "rgba(200,200,200,0.5)";
+            txt.useHandCursor = true;
+        };
+        var onOut = function (target) {
+            target.fill = "white";
+            target.stroke = "rgba(0,0,0,0)";
+            txt.useHandCursor = false;
+        };
+        //txt.useHandCursor = true;
+        txt.inputEnabled = true;
+        txt.events.onInputUp.add(callback, this);
+        txt.events.onInputOver.add(onOver, this);
+        txt.events.onInputOut.add(onOut, this);
+
+        this.optionCount++;
+    },
+
+    create: function () {
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.swipe = new Swipe(this.game);
+
+        this.game.stage.backgroundColor = "#4488AA";
+
+        map = this.game.add.tilemap('map');
+        map.addTilesetImage('water');
+        map.setCollisionBetween(1, 99);
+        this.game.physics.enable(map, Phaser.Physics.ARCADE);
+
+
+
+        layer = map.createLayer('level1');
+        layer.resizeWorld();
+        layer.wrap = true;
+
+        Floor = this.game.add.group();
+        Floor.enableBody = true;
+        this.game.physics.enable(Floor, Phaser.Physics.ARCADE);
+
+
+        Frog = this.game.add.group();
+        Frog.enableBody = true;
+        this.game.physics.enable(Frog, Phaser.Physics.ARCADE);
+
+        map.createFromObjects('obj1', 2, 'floor', 0, true, false, Floor);
+
+        console.log('Anzahl Boden: ' + Floor.length);
+
+        sprite = this.game.add.sprite(300, 930, 'frog');
+        sprite.anchor.setTo(0.5, 0.5);
+        this.game.physics.arcade.enable(sprite);
+
+        Frog.callAll('animations.add', 'animations', 'frog', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 10, true);
+        Frog.callAll('animations.play', 'animations', 'frog');
+
+        water = this.game.add.tileSprite(0, 150, 750, 850, 'water');
+        water.animations.add('waves0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+
+        var n = 0;
+        water.animations.play('waves' + n, 8, true);
+
+        this.game.camera.follow(sprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
+        this.game.world.bringToTop(Floor);
+        this.game.world.bringToTop(sprite);
+
+        stateText = game.add.text(game.world.centerX, game.world.centerY, ' ', {font: '60px Arial', fill: '#ff0000'});
+        stateText.anchor.setTo(0.9, 1.7);
+        stateText.visible = false;
+        stateText.fixedToCamera = true;
+
+    },
+
+    trap: function (sprite, Floor) {
+        var tween = game.add.tween(Floor).to({kill: true}, 1000, Phaser.Easing.Linear.None, true);
+        tween.onComplete.add(function () {
+            Floor.destroy();
+        });
+    },
+
+    checkOverlap: function (sprite, Floor) {
+        var boundsA = sprite.getBounds();
+        var boundsB = Floor.getBounds();
+
+        return Phaser.Rectangle.intersects(boundsA, boundsB);
+    },
+
+    update: function () {
+        this.game.physics.arcade.overlap(Floor, sprite, this.trap);
+
+        var direction = this.swipe.check();
+        if (direction !== null) {
+            // case this.swipe.DIRECTION_UP: sprite.y -= 50; break;
+            switch (direction.direction) {
+
+                case this.swipe.DIRECTION_UP:
+                    sprite.y -= 57;
+                    break;
+                case this.swipe.DIRECTION_LEFT:
+                    sprite.x -= 50;
+                    break;
+                case this.swipe.DIRECTION_RIGHT:
+                    sprite.x += 50;
+                    break;
+                case this.swipe.DIRECTION_DOWN:
+                    sprite.y += 57;
+                    break;
+                case this.swipe.DIRECTION_UP_LEFT:
+                    sprite.y -= 50;
+                    sprite.x -= 50;
+                    break;
+                case this.swipe.DIRECTION_UP_RIGHT:
+                    sprite.y -= 50;
+                    sprite.x += 50;
+                    break;
+                case this.swipe.DIRECTION_DOWN_LEFT:
+                    sprite.y += 50;
+                    sprite.x -= 50;
+                    break;
+                case this.swipe.DIRECTION_DOWN_RIGHT:
+                    sprite.y += 50;
+                    sprite.x += 50;
+                    break;
+            }
+        }
+
+        if (this.swipe.DIRECTION_UP.isDown) {
+            worldScale += 0.05;
+        }
+        else if (this.swipe.DIRECTION_UP.isDown) {
+            worldScale -= 0.05;
+        }
+
+        if (this.checkOverlap(sprite, Floor)) {
+            //
+        }
+        else {
+
+            this.game.state.start("GameOver");
+            /*
+            sprite.kill();
+            stateText.text = " GAME OVER \n Click to restart";
+            stateText.visible = true;
+
+            game.input.onTap.addOnce(function () {
+                game.state.restart();
+            }, this);
+            */
+        }
+
+
+    },
+
+    render: function () {
+
+
+        game.time.advancedTiming = true;
+        game.debug.text(game.time.fps, 10, 15, "#00ff00");
+    }
+
+};
